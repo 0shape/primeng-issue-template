067bea234ab594709b916f3bae5c1c3a3feb3b6a
denis.shapov <0shape@gmail.com>
2018-11-30 15:37:53 +0300

    fix solution comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,18 +23,18 @@ export class AppComponent {
 
 
 
-Dropdown.prototype.findOptionIndex = function (val, opts) {
-  var index = -1;
-  if (opts) {
-      for (var i = 0; i < opts.length; i++) {
-          if ((val == null && opts[i].value == null)
-              || this.objectUtils.equals(val, opts[i].value, this.dataKey)
-              || opts[i].value[this.dataKey] == val) { // solution
-              index = i;
-              break;
-          }
-      }
-  }
-  console.log('Dropdown.prototype.findOptionIndex\n','val',val,'index', index,'opts\n',opts)
-  return index;
-};
+// Dropdown.prototype.findOptionIndex = function (val, opts) {
+//   var index = -1;
+//   if (opts) {
+//       for (var i = 0; i < opts.length; i++) {
+//           if ((val == null && opts[i].value == null)
+//               || this.objectUtils.equals(val, opts[i].value, this.dataKey)
+//               || opts[i].value[this.dataKey] == val) { // solution
+//               index = i;
+//               break;
+//           }
+//       }
+//   }
+//   console.log('Dropdown.prototype.findOptionIndex\n','val',val,'index', index,'opts\n',opts)
+//   return index;
+// };
